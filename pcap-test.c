9c7448a3994530c3640578a48f752e84a4296d15
seunghye <129871487+bsh8798@users.noreply.github.com>
2025-03-17 02:03:24 -0400

    Update pcap-test.c

--- pcap-test.c
+++ pcap-test.c
@@ -94,25 +94,25 @@ int main(int argc, char* argv[]) {
     struct payload *payload;
 
 	while (true) {
-        struct pcap_pkthdr* header;
-		const u_char* packet;
-		int res = pcap_next_ex(pcap, &header, &packet);
-		if (res == 0) continue;
-		if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
-			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
-			break;
-		}
-        //printf("%u bytes captured\n", header->caplen);
-
-        ether_hdr = (struct ethernet_hdr*)packet;
-        ip_hdr = (struct ip_hdr*)(packet + sizeof(struct ethernet_hdr));
-        tcp_hdr = (struct tcp_hdr*)(packet + sizeof(struct ethernet_hdr) + ip_hdr->IHL*4);
-        payload = (struct payload*)(packet + sizeof(struct ethernet_hdr) + ip_hdr->IHL*4 + tcp_hdr->header_length*4);
-        uint payload_length = header->caplen - (sizeof(struct ethernet_hdr) + ip_hdr->IHL*4 + tcp_hdr->header_length*4);
-
-        if(ip_hdr->protocol == 0x06) {
-            printInfo(ether_hdr, ip_hdr, tcp_hdr, payload, payload_length);
-        }
+	        struct pcap_pkthdr* header;
+			const u_char* packet;
+			int res = pcap_next_ex(pcap, &header, &packet);
+			if (res == 0) continue;
+			if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
+				printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
+				break;
+			}
+	        //printf("%u bytes captured\n", header->caplen);
+	
+	        ether_hdr = (struct ethernet_hdr*)packet;
+	        ip_hdr = (struct ip_hdr*)(packet + sizeof(struct ethernet_hdr));
+	        tcp_hdr = (struct tcp_hdr*)(packet + sizeof(struct ethernet_hdr) + ip_hdr->IHL*4);
+	        payload = (struct payload*)(packet + sizeof(struct ethernet_hdr) + ip_hdr->IHL*4 + tcp_hdr->header_length*4);
+	        uint payload_length = header->caplen - (sizeof(struct ethernet_hdr) + ip_hdr->IHL*4 + tcp_hdr->header_length*4);
+	
+	        if(ip_hdr->protocol == 0x06) {
+	            printInfo(ether_hdr, ip_hdr, tcp_hdr, payload, payload_length);
+	        }
 	}
 
 	pcap_close(pcap);
